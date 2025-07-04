a1f98bcac3f3db4b67dc6698d0f0bd361d6ac231
Sneha <snekarki80@gmail.com>
2025-07-04 14:33:52 +0545

    login fixed

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ USE_I18N = True
 import os
 STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR / 'static'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  
 ]
